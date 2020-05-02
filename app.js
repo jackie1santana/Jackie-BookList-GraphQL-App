c732bab66cf57c2a1ed1a6d520f8c4a3138e46e5
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-05-02 12:39:59 -0500

    s

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
+const path = require('path')
  const app = express()
- app.use(cors())
-
- const path = require('path')
-const publicDirectoryPath = path.join(__dirname, '/client/build')
-app.use(express.static(publicDirectoryPath))
 
  const port = process.env.PORT
+
+ app.use(cors())
+
+
+const publicDirectoryPath = path.join(__dirname, './client/build')
+
+app.use(express.static(publicDirectoryPath))
+
+
  
  mongoose.connect('mongodb+srv://jackie:1234@fcc-graphql-ikerj.mongodb.net/graphql?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
